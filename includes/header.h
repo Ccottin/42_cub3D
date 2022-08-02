b7351c2268528795f092965ca24db0d4abbad037
Clara COTTIN <ccottin@e1r11p8.clusters.42paris.fr>
2022-08-03 00:23:01 +0200

    1

--- includes/header.h
+++ includes/header.h
@@ -2,6 +2,18 @@
 # define HEADER_H
 
 # include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
 # include <mlx.h>
+
+typedef struct s_win {
+	void	*mlx;
+	void	*win;
+	int		width;
+	int		height;
+	}	t_win;
+
+typedef struct s_data {
+	}	t_data;
 
 #endif
