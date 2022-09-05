830fe5e455dd7f4961e4ce62630d786e6d6fe7d9
ccottin <cottin.clara7@gmail.com>
2022-09-05 22:29:15 +0200

    fix texures orientation pb

--- srcs/raycaster.c
+++ srcs/raycaster.c
@@ -54,6 +54,7 @@ void	set_search(t_data *data, double *distx, double *disty, int *wall)
 	else
 		data->caster.addy = fabs(1 / data->caster.raydiry);
 	get_dist(data, distx, disty);
+//	printf("raydir = %f %f\n", data->caster.addx, data->caster.addy);
 }
 
 void	search_wall(t_data *data, double *distx, double *disty)
@@ -75,25 +76,42 @@ void	search_wall(t_data *data, double *distx, double *disty)
 			data->caster.side = -1;
 			data->caster.casey += data->caster.diry;
 		}
-	//	printf("casex = %d case y = %d c = %c\n", data->caster.casex, data->caster.casey, data->map.map[data->caster.casey][data->caster.casex]);
 		if (data->map.map[data->caster.casey][data->caster.casex] && 
 			data->map.map[data->caster.casey][data->caster.casex] == '1')
 			wall = 1;
 	}
+//	printf("%d; %d, %c\n", data->caster.casey, data->caster.casex, data->map.map[data->caster.casey][data->caster.casex]);
 	if (data->caster.side == 1)
 		*distx -= data->caster.addx;
 	else
 		*disty -= data->caster.addy;
 }
 
-void	get_texture(t_data *data)//disty = -1
+t_img	*get_texture(t_data *data)
 {
+	t_img	*img;
+
+	if (data->caster.side == 1)
+	{
+		if (data->caster.dirplayerx < 0)
+			img = &data->east;
+		else
+			img = &data->west;
+	}
+	else
+	{
+		if (data->caster.dirplayery < 0)
+			img = &data->south;
+		else	
+			img = &data->north;
+	}
 	data->caster.wallhit -= (int)data->caster.wallhit;
-	data->caster.texposx = (double)data->north.width * data->caster.wallhit;
+	data->caster.texposx =(int)((double)img->width * data->caster.wallhit);
 	if ((data->caster.side == -1 && data->caster.raydiry < 0)
 		||(data->caster.side == 1 && data->caster.raydirx > 0))
-		data->caster.texposx = data->north.width - data->caster.texposx - 1;
-
+		data->caster.texposx = img->width - data->caster.texposx
+			- 1;
+	return (img);
   }
 
 unsigned long createRGB(int r, int g, int b)
@@ -108,37 +126,56 @@ void	draw_ceiling_floor(int start, t_data *data, int x, int end)
 	i = 0;
 	while (i < start)
 	{
-		pixel_to_image(data, x, i, createRGB(data->map.ceiling_color.red, data->map.ceiling_color.green, data->map.ceiling_color.blue));
+		pixel_to_image(data, x, i, createRGB(data->map.ceiling_color.red,
+			data->map.ceiling_color.green,
+			data->map.ceiling_color.blue));
 		i++;
 	}
 	while (i < end)
 		i++;
 	while (i < data->caster.screen_w)
 	{
-		pixel_to_image(data, x, i, createRGB(data->map.floor_color.red, data->map.floor_color.green, data->map.floor_color.blue));
+		pixel_to_image(data, x, i, createRGB(data->map.floor_color.red,
+		data->map.floor_color.green,
+		data->map.floor_color.blue));
 		i++;
 	}
 }
 
-int	get_color(t_data *data)
+int	get_color(t_data *data, t_img *img)
 {
 	int	color;
 	int	rgb;
 
-	color = *(int*)(data->north.addr
-		+ ((int)data->caster.texposy * data->north.height
+	color = *(int*)(img->addr
+		+ ((int)data->caster.texposy * img->height
 		+ (int)data->caster.texposx) * 4);
 	rgb = (color & 0xFF0000) | (color & 0x00FF00) | (color & 0x0000FF);
 	return (rgb);
 }
+
+void	draw_line2(t_data *data, int *start, int x, t_img *img)
+{
+	int	color;
+
+	if (data->caster.texposy >= img->height)
+		data->caster.texposy = img->height - 1;
+  	color = get_color(data, img);
+	if (data->caster.side == 1)
+		pixel_to_image(data, x, *start, (color >> 1) & 8355711);
+	else
+		pixel_to_image(data, x, *start, color);
+	data->caster.texposy += data->caster.stepy;
+	(*start)++;
+}
+
 
 void	draw_line(t_data *data, double dist, int x)
 {
 	float	line;
 	int	start;
 	int	end;
-	double	stepy;
-	int	color;
+	t_img	*img;
 
 	line = ((float)data->caster.screen_w / dist); 	
 	start = data->caster.middle_w - line / 2;
@@ -147,22 +184,23 @@ void	draw_line(t_data *data, double dist, int x)
 		start = 0;
 	if (end > data->caster.screen_w)
 		end = data->caster.screen_w - 1;
-	get_texture(data);
-	stepy = 1.0 * (double)data->north.height / line;
+	img = get_texture(data);
 	draw_ceiling_floor(start, data, x, end);
-	data->caster.texposy = (start - data->caster.middle_w + line / 2 + 1) * stepy;
+	data->caster.stepy = 1.0 * (double)img->height / line;
+	data->caster.texposy = (start - data->caster.middle_w + line / 2 + 1)
+		* data->caster.stepy;
 	while (start < end)
-  	{
-		if (data->caster.texposy >= data->north.height)
-			data->caster.texposy = data->north.height - 1;
-	  	color = get_color(data);
-		if (data->caster.side == 1)
-			pixel_to_image(data, x, start, (color >> 1) & 8355711);
-		else
-			pixel_to_image(data, x, start, color);
-		data->caster.texposy += stepy;
-		start++;
-	}
+  		draw_line2(data, &start, x, img);
+}
+
+void	get_raydir(t_data *data, int start)
+{
+	data->caster.camerax = 2 * start
+		/ (double)data->caster.screen_l - 1;
+	data->caster.raydirx = data->caster.dirplayerx
+		+ data->caster.planex * data->caster.camerax;
+	data->caster.raydiry = data->caster.dirplayery
+		+ data->caster.planey * data->caster.camerax;	
 }
 
 int	get_img(t_data *data)
@@ -172,25 +210,22 @@ int	get_img(t_data *data)
 	double	disty;
 
 	start = 0;
-	if (init_img(&(data->img0), data, 0)) //modifier en fonction de auelle image on dessine
+	if (init_img(&(data->img0), data, 0))
 		return (-1);
 	while (start < data->caster.screen_l)
 	{
-		data->caster.camerax = 2 * start
-			/ (double)data->caster.screen_l - 1;	
-		data->caster.raydirx = data->caster.dirplayerx
-			+ data->caster.planex * data->caster.camerax;
-		data->caster.raydiry = data->caster.dirplayery
-			+ data->caster.planey * data->caster.camerax;
+		get_raydir(data, start);
 		search_wall(data, &distx, &disty);
 		if (data->caster.side == 1)
 		{
-			data->caster.wallhit = data->caster.playery + distx * data->caster.raydiry;
+			data->caster.wallhit = data->caster.playery
+				+ distx * data->caster.raydiry;
 			draw_line(data, distx, start);
 		}
 		else
 		{
-			data->caster.wallhit = data->caster.playerx + disty * data->caster.raydirx;
+			data->caster.wallhit = data->caster.playerx
+				+ disty * data->caster.raydirx;
 			draw_line(data, disty, start);
 		}
 		set_null_caster(data);
@@ -199,21 +234,41 @@ int	get_img(t_data *data)
 	return (0);
 }
 
+int	init_texture(t_data *data)
+{
+	data->north.img = mlx_xpm_file_to_image(data->win.mlx, 
+	data->map.north_texture, &data->north.width, &data->north.height);
+	data->south.img = mlx_xpm_file_to_image(data->win.mlx, 
+	data->map.south_texture, &data->south.width, &data->south.height);
+	data->east.img = mlx_xpm_file_to_image(data->win.mlx, 
+	data->map.east_texture, &data->east.width, &data->east.height);
+	data->west.img = mlx_xpm_file_to_image(data->win.mlx, 
+	data->map.west_texture, &data->west.width, &data->west.height);
+	if (!data->north.img || !data->south.img || !data->east.img
+		|| !data->west.img)
+		return (-1);
+	if (init_img(&data->south, data, 1))
+		return (-1);
+	if (init_img(&data->north, data, 1))
+		return (-1);
+	if (init_img(&data->east, data, 1))
+		return (-1);
+	if (init_img(&data->west, data, 1))
+		return (-1);
+	return (0);
+}
+
 int	raycaster(t_data *data)
 {
 	set_caster(data);
 	if (init_mlx(data))
 		return (-1);
-	data->north.img = mlx_xpm_file_to_image(data->win.mlx, "japtext/japset1_1.xpm",
-			&data->north.width, &data->north.height);
-	if (!data->north.img)
-		return (-1);
-	if (init_img(&data->north, data, 1))
+	if (init_texture(data))
 		return (-1);
 	if (get_img(data))
 		return (-1);
-	mlx_put_image_to_window(data->win.mlx, data->win.win, data->img0.img, 0, 0);
-	mlx_put_image_to_window(data->win.mlx, data->win.win, data->north.img, 0, 0);
+	mlx_put_image_to_window(data->win.mlx, data->win.win, data->img0.img,
+		0, 0);
 	events_handler(data);
 	mlx_loop(data->win.mlx);
 	return (0);
